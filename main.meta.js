9f2375e84cc5a5923f11224146b393adb6ccb530
werunopi <werunopi@gmail.com>
2026-01-04 20:10:35 +0500

    Create main.meta.js

---
+++ main.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         NSPD Parser
+// @namespace    https://github.com/werunopi/NSPD-Parser
+// @updateURL    https://github.com/werunopi/NSPD-Parser/main.user.js
+// @version      1.0
+// ==/UserScript==
